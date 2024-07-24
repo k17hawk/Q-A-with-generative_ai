6e61eaa16781473af754516b0c146980100ea057
dahalkumar <kumardahal536@gmail.com>
2024-07-24 17:11:11 -0400

    llama3 model Q&A app

---
+++ app.py
@@ -0,0 +1,61 @@
+import streamlit as st
+import openai
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Langsmith tracking
+os.environ['LANGCHAIN_TRACING_V2'] = "true"
+os.environ['LANGCHAIN_PROJECT'] = "Q&A ChatBot with OpenAI"
+
+# Prompt template
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful assistant. Please respond to user queries"),
+        ("user", "Question:{question}")
+    ]
+)
+
+# Generate response from user question based on API, engine
+# Temperature is level of creativity, 0 is less creative
+def generate_response(question, api_key, llm, temperature, max_tokens):
+    openai.api_key = api_key
+    llm = ChatOpenAI(model=llm, openai_api_key=api_key)
+    # Output parser
+    output_parser = StrOutputParser()
+    chain = prompt | llm | output_parser
+
+    answer = chain.invoke({'question': question})
+    return answer
+
+# Title of the app
+st.title("Enhanced Q&A Chatbot with OpenAI")
+
+# Sidebar for settings
+st.sidebar.title("Settings")
+get_api_key = st.sidebar.text_input("Enter your OpenAI API key:", type="password")
+
+# Dropdown to select the models
+llm = st.sidebar.selectbox("Select an OpenAI model", ["gpt-4", "gpt-4-turbo", "gpt-4o"])
+
+# Adjust the response parameters
+temperature = st.sidebar.slider("Temperature", min_value=0.0, max_value=1.0, value=0.7)
+max_tokens = st.sidebar.slider("Max Tokens", min_value=50, max_value=300, value=150)
+
+# Main interface for user input
+st.write("Go ahead and ask me anything")
+user_input = st.text_input("You:")
+
+if user_input:
+    if get_api_key:
+        response = generate_response(user_input, get_api_key, llm, temperature, max_tokens)
+        st.write(response)
+    else:
+        st.write("Please provide the API key")
+else:
+    st.write("Please provide a query")
